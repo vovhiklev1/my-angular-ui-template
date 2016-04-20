9675141737965a5754b5c5ca39e9f598a5defeb6
vovhiklev1main <responsibilities@i.ua>
2016-04-20 22:14:24 +0300

    default settings

--- app/scripts/config/states.js
+++ app/scripts/config/states.js
@@ -8,7 +8,7 @@
    /// config.$inject = [ '$stateProvider', '$urlRouterProvider'];
 
     function config( $stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.when('', 'main');
+        $urlRouterProvider.when('/', 'main');
 
         $stateProvider
             .state('main', {
