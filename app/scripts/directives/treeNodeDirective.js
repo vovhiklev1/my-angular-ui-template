f0896f851ff6dc4238c982995a2e664255ff1d62
vovhiklev1main <responsibilities@i.ua>
2016-04-24 14:25:41 +0300

    tree alive

--- app/scripts/directives/treeNodeDirective.js
+++ app/scripts/directives/treeNodeDirective.js
@@ -7,19 +7,28 @@
     function treeNodeDirective(tree) {
         return {
             restrict: 'AEC',
+            transclude: true,
+            replace: true,
             scope: {
                 treeNode: '=treeNode'
             },
-            template: '<ul>' +
+            template:  '<ul class="uuuuul">' +
             '<li ng-repeat="node in treeNode">' +
             '<a ng-click="get(node)">{{node.name}}</a>' +
-            '<div tree-node="node.nodes"></div>' +
+                 '<div tree-node="node.nodes"></div>' +
             '</li>' +
-            '</ul>',
+            '</ul>' ,
+
             link: link
         };
 
-        function link(scope, elem, attr) {
+        function link(scope, elem, attr, ctrl, transclude) {
+
+            transclude(function (clone) {
+                elem.after(clone)
+            });
+
+
             scope.get = function (node) {
                 tree.getNodes(node.id).then(function (res) {
                     node.nodes = res;
